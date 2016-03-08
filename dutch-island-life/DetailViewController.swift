3a9d8253aed7180f7684e713ca00afc2cb35789f
Dide van Berkel <didevanberkel@MacBook-Air-van-Dide.local>
2016-03-08 19:53:21 +0100

    Added files Dutch Island Life

---
+++ dutch-island-life/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailVCViewController.swift
+//  dutch-island-life
+//
+//  Created by Dide van Berkel on 06-03-16.
+//  Copyright © 2016 Gary Grape Productions. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- dutch-island-life/ViewController.swift
+++ dutch-island-life/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var moreBtn: UIButton!
+    @IBOutlet weak var photosBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        moreBtn.layer.cornerRadius = 5.0
+        photosBtn.layer.cornerRadius = 5.0
+        
     }
 
     override func didReceiveMemoryWarning() {
